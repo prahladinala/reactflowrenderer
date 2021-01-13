904ab8d8a0006cdecd13c30f996471a41092282f
prahladinala <prahladinala@users.noreply.github.com>
2021-01-13 05:52:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import MindNode from './components/node.components'
+
+function App() {
+  return (
+    <div className='App'>
+      <MindNode />
+    </div>
+  )
+}
+
+export default App
